513b5e4cb143200632697bb61cfd900e69aa2295
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-24 23:05:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,13 @@
 require_relative "./user.rb"
 class Student < User 
+  
+  attr_accessor :knowledge 
+  
+  def initialize(knowledge)
+    @knowledge = []
+  end
+  
+  def learn 
+    @knowledge << 
 
 end
